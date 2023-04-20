59d9dd13e8868abd1e270eea8a9b6cd0a9f711b8
キャンド <amehikoji@gmail.com>
2023-04-19 20:39:43 -0400

    Use anilist wrapper

--- auto_update.py
+++ auto_update.py
@@ -1,28 +1,9 @@
-import requests
 import json
 import time
-
-url = 'https://graphql.anilist.co'
+import Anilist
 
 with open("animes.json", 'r') as f:
     animes = json.loads(f.read())
-
-with open("code.txt", "r", encoding='utf-8') as f:
-    code = f.read()
-
-with open("secret.txt", "r", encoding='utf-8') as f:
-    secret = f.read()
-
-
-c = requests.post("https://anilist.co/api/v2/oauth/token", json={
-    'grant_type': 'authorization_code',
-    'client_id': 12213,
-    'client_secret': secret,
-    'redirect_uri': 'https://anilist.co/api/v2/oauth/pin',
-    'code': code
-}, headers={'Accept': 'application/json'}).json()
-
-access_token = c["access_token"]
 
 with open("elo_raw.json", "r") as f:
     elo = json.loads(f.read())
@@ -37,30 +18,11 @@ lwst = list(l.items())[-1][1]
 hghst = list(l.items())[0][1]-lwst
 l = {k: int(100*(v-lwst)/(hghst)) for k, v in l.items()}
 
+auth = Anilist.Auth.from_config_file("config.json")
+mutate_client = Anilist.MutationClient(auth)
+
 for k, v in l.items():
-    if k not in [anime[2] for anime in animes]:
-        continue
-    query = '''
-    mutation ($mediaId: Int, $score: Float) {
-        SaveMediaListEntry (mediaId: $mediaId, score: $score) {
-            id
-            score
-        }
-    }
-    '''
+    
+    mutate_client.media_entry(k).set_score(v)
 
-    k = requests.post(url, json={'query': query, 'variables': {'mediaId': k, 'score': v}}, headers={'Authorization': f"Bearer {access_token}", 'Content-Type': 'application/json', 'Accept': 'application/json'}).json()
-    #print(k)
-    id = k["data"]["SaveMediaListEntry"]["id"]
-
-    query = '''
-    mutation ($id: Int, $score: Float) {
-        SaveMediaListEntry (id: $id, score: $score) {
-            id
-            score
-        }
-    }
-    '''
-    k = requests.post(url, json={'query': query, 'variables': {'id': id, 'score': v}}, headers={'Authorization': f"Bearer {access_token}", 'Content-Type': 'application/json', 'Accept': 'application/json'}).json()
-    print(k)
     time.sleep(1.5)
