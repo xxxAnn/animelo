018b4c7a0fee34d20e794213cfa5ad0806721a0a
キャンド <amehikoji@gmail.com>
2023-04-19 20:32:40 -0400

    use Anilist wrapper

--- get_animes.py
+++ get_animes.py
@@ -1,46 +1,10 @@
-import requests, json
+import Anilist, json
 
-USERNAME = "xxxAnn"
+client = Anilist.QueryClient()
 
-query = '''
-query ($usr: String, $page: Int, $perPage: Int) { 
-  Page (page: $page, perPage: $perPage) {
-        mediaList (userName: $usr) {
-            media {
-                id
-                title {
-                    romaji
-                    english
-                }
-                coverImage {
-                    extraLarge
-                }
-            }
-        }
-    }
-}
-'''
+media_list = client.media_list(username="xxxAnn", languages=["romaji", "english"])
 
-variables = {
-    'usr': USERNAME,
-    'page': 1,
-    'perPage': 10
-}
-
-url = 'https://graphql.anilist.co'
-
-i = 1
-
-d = []
-
-while True:
-    data = [(r["media"]["title"]["romaji"], r["media"]["title"]["english"], r["media"]["id"], r["media"]["coverImage"]["extraLarge"]) for r in 
-            requests.post(url, json={'query': query, 'variables': variables}).json()["data"]["Page"]["mediaList"]]
-    if data == []:
-        break
-    d.extend(data)
-    i+=1
-    variables['page'] = i
+d = [[el.title.romaji, el.title.english, el.id, el.coverImage.extraLarge] for el in media_list.entries]
 
 with open("animes.json", "w") as f:
     f.write(json.dumps(d, indent=4))
